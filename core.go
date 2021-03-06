80e174c68f939b7c6d0430de8cf5df94fcee3425
oreyes <oreyes@grupotecun.com>
2021-03-06 11:02:16 -0600

    first commit

---
+++ core.go
@@ -0,0 +1,75 @@
+package core
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//MODEL
+
+//Res model
+type Res struct {
+	Ok      bool        `json:"ok"`
+	Res     interface{} `json:"res,omitempty"`
+	Message string      `json:"message,omitempty"`
+}
+
+//User ...
+type User struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty" json:"id,omitempty"`
+	Name     string             `json:"name"`
+	LastName string             `json:"lastName"`
+	Email    string             `json:"email"`
+	Password string             `json:"password"`
+	Admin    bool               `json:"admin"`
+	Create   time.Time          `json:"create"`
+	Update   time.Time          `json:"update"`
+}
+
+//Shop ...
+type Shop struct {
+	ID      primitive.ObjectID `json:"id"`
+	Name    string             `json:"name"`
+	Address string             `json:"address"`
+	Coin    string             `json:"coin"`
+	Create  time.Time          `json:"create"`
+	Update  time.Time          `json:"update"`
+}
+
+//Db ...
+func Db(collection string) *mongo.Collection {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.puckd.mongodb.net/test"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client.Database("minim").Collection(collection)
+}
+
+//CRUD
+
+//Find ...
+func Find(collection string) ([]interface{}, error) {
+	res, err := Db(collection).Find(context.TODO(), bson.D{{}})
+	results := make([]interface{}, 0)
+	for res.Next(context.TODO()) {
+		var obj interface{}
+		err := res.Decode(&obj)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &obj)
+	}
+	res.Close(context.TODO())
+	return results, err
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/areyesoleon/go-playground-core
+
+go 1.16
+
+require go.mongodb.org/mongo-driver v1.4.6 // indirect
