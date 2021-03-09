11e7dfa78ff0c4214d608fe26aa36c78e8bb9761
oreyes <oreyes@grupotecun.com>
2021-03-09 17:25:21 -0600

    :hammer: mejora en estructuras

--- core.go
+++ core.go
@@ -10,6 +10,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var singleTonMaster *MasterStruct
+
 //MODEL
 
 //Res model
@@ -19,26 +21,87 @@ type Res struct {
 	Message string      `json:"message,omitempty"`
 }
 
+// TimeData ...
+type timeData struct {
+	Create time.Time `json:"create"`
+	Update time.Time `json:"update"`
+}
+
 //User ...
 type User struct {
-	ID       primitive.ObjectID `bson:"_id, omitempty" json:"id,omitempty"`
+	ID       primitive.ObjectID `bson:"_id, omitempty" json:"id,omitempty,unique"`
 	Name     string             `json:"name"`
 	LastName string             `json:"lastName"`
 	Email    string             `json:"email"`
 	Password string             `json:"password"`
 	Admin    bool               `json:"admin"`
-	Create   time.Time          `json:"create"`
-	Update   time.Time          `json:"update"`
+	*timeData
 }
 
-//Shop ...
-type Shop struct {
+type placeStore struct {
 	ID      primitive.ObjectID `json:"id"`
 	Name    string             `json:"name"`
 	Address string             `json:"address"`
 	Coin    string             `json:"coin"`
-	Create  time.Time          `json:"create"`
-	Update  time.Time          `json:"update"`
+}
+
+//Shop ...
+type Shop struct {
+	*placeStore
+	*timeData
+}
+
+//Kiosk ...
+type Kiosk struct {
+	*placeStore
+	IDshop primitive.ObjectID `json:"idShop"`
+	*timeData
+}
+
+//MasterStruct ...
+type MasterStruct struct {
+	IDuser  primitive.ObjectID `json:"idUser"`
+	IDshop  primitive.ObjectID `json:"idShop"`
+	IDkiosk primitive.ObjectID `json:"idKiosk"`
+}
+
+//SingleTonMaster ...
+func SingleTonMaster() *MasterStruct {
+	if singleTonMaster == nil {
+		singleTonMaster = &MasterStruct{}
+	}
+
+	return singleTonMaster
+}
+
+//SetIDUser set user id
+func (m *MasterStruct) SetIDUser(id primitive.ObjectID) {
+	m.IDuser = id
+}
+
+//GetIDUser user id
+func (m *MasterStruct) GetIDUser() primitive.ObjectID {
+	return m.IDuser
+}
+
+//SetIDShop shop user id
+func (m *MasterStruct) SetIDShop(id primitive.ObjectID) {
+	m.IDshop = id
+}
+
+//GetIDShop shop id
+func (m *MasterStruct) GetIDShop() primitive.ObjectID {
+	return m.IDshop
+}
+
+//SetIDkiosk set kiosk id
+func (m *MasterStruct) SetIDkiosk(id primitive.ObjectID) {
+	m.IDkiosk = id
+}
+
+//GetIDkiosk kiosk id
+func (m *MasterStruct) GetIDkiosk() primitive.ObjectID {
+	return m.IDkiosk
 }
 
 //Db ...
